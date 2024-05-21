a6a914774496f69f01d0892ac9ce3e355e231cde
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-05-21 21:57:23 +0900

    DBのスキーマとモデル実装した

---
+++ src/models/task.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document } from "mongoose";
+
+export interface Task {
+    title: StaticRangeInit;
+    desciption: string;
+    dueDate: string;
+    isComppleted: boolean;
+}
+
+export interface TaskDocument extends Task, Document {
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const taskSchema = new mongoose.Schema<TaskDocument>({
+    title: {
+        type: String,
+        required: true,
+    },
+    desciption: { 
+        type: String,
+    },
+    dueDate: {
+        type: String,
+        required: true,
+    },
+    isComppleted: {
+        type: Boolean,
+        default: false,
+    }
+}, { timestamps: true });
+
+// すでにモデルが作成されている場合は左辺でモデルを取得して、作成されていない場合がschemaからモデルを作成する
+export const TaskModel = mongoose.models.Task || mongoose.model('Task', taskSchema);
